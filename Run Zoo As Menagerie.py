42f27a66b26709f11508d811acf4b9ca9a901317
EllaSepetjian <89997385+EllaSepetjian@users.noreply.github.com>
2022-07-04 11:52:15 -0400

    Add files via upload

---
+++ Run Zoo As Menagerie.py
@@ -0,0 +1,7 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/ellas/OneDrive/Desktop/IvyTech Fall Assignments 2021 Part 1/Ivy Tech Summer 2022/SDEV 220/zoo.py
+import zoo as menagerie
+ZooHours = menagerie.hours()
+Open 9-5 daily

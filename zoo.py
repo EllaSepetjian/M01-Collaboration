bfe40b369746b9bf86ff567564411f33584d2f4a
EllaSepetjian <89997385+EllaSepetjian@users.noreply.github.com>
2022-07-04 11:33:49 -0400

    Add files via upload

---
+++ zoo.py
@@ -0,0 +1,13 @@
+# Ella Sepetjian
+# SDEV 220
+# Assignment 11.1 from chapter 11
+
+def hours():
+    print('Open 9-5 daily')
+
+''' Next run this file and type -
+import zoo
+ZooHours = zoo.hours()
+
+Then the output will be =
+Open 9-5 daily '''

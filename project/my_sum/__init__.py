1df4dc763b82b082d3cd15f60ccddca5c68ccc23
EllaSepetjian <89997385+EllaSepetjian@users.noreply.github.com>
2022-07-11 11:48:17 -0400

    Add files via upload

---
+++ project/my_sum/__init__.py
@@ -0,0 +1,9 @@
+# Ella Sepetjian
+# SDEV 220
+# M05 Programming Assignment - Writing Your First Test Part1
+
+def sum(arg):
+    total = 0
+    for val in arg:
+        total += val
+    return total
